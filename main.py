08432f1674630521b1ad958489aa064ce4b37fef
Junkyo <jkseo50@naver.com>
2020-08-08 12:12:02 +0900

    time check, youtube update

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import cv2
-import numpy as np
 import dlib
-from math import hypot
+import numpy as np
+import pafy
+import time
 
+concentration = 100
+face_frequency = []
+eye_center_frequency = []
+eye_right_frequency = []
+eye_left_frequency = []
+time_flag = 0
+url = "https://www.youtube.com/watch?v=PPLop4L2eGk"
+youtube_video = pafy.new(url)
+best = youtube_video.getbest(preftype='mp4')
 cap = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(best.url)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-def midpoint(p1 ,p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
+
 
 font = cv2.FONT_HERSHEY_PLAIN
 
-def get_blinking_ratio(eye_points, facial_landmarks):
-    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
-    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
-    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
-    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
-
-    #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-    #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
-
-    hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
-    ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
-
-    ratio = hor_line_lenght / ver_line_lenght
-    return ratio
 
 def get_gaze_ratio(eye_points, facial_landmarks):
     left_eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
@@ -34,8 +33,8 @@ def get_gaze_ratio(eye_points, facial_landmarks):
                                 (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
                                 (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
                                 (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
-                                (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], np.int32)
-    # cv2.polylines(frame, [left_eye_region], True, (0, 0, 255), 2)
+                                (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)],
+                               np.int32)
 
     height, width, _ = frame.shape
     mask = np.zeros((height, width), np.uint8)
@@ -65,96 +64,54 @@ def get_gaze_ratio(eye_points, facial_landmarks):
         gaze_ratio = left_side_white / right_side_white
     return gaze_ratio
 
-while True:
-    _, frame = cap.read()
 
+first_time = time.time()
+print(first_time)
+while True:
+    second_time = time.time()
+    # print(second_time - first_time)
+    time_flag += 1
+    _, frame = cap.read()
+    _, frame2 = cap2.read()  # youtube
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    #인식된 얼굴 갯수를 출력
-    print(len(faces))
+    # 인식된 얼굴 갯수를 출력
+    # print(len(faces))
+    if 1.9 <= abs(second_time - first_time) < 2.1:
+        first_time += 1
+        face_frequency.append(len(faces))
+        if face_frequency[-1] == 0:
+            concentration -= 0.25
 
     # 인식된 얼굴에 사각형을 출력한다
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-#화면에 출력한다
-    cv2.imshow('frame', frame)
-    new_frame = np.zeros((500, 500, 3), np.uint8)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('youtube', frame2)
 
     faces = detector(gray)
     for face in faces:
-        #x, y = face.left(), face.top()
-        #x1, y1 = face.right(), face.bottom()
-        #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
-
         landmarks = predictor(gray, face)
 
-        # # Detect blinking
-        # left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
-        # right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
-        # blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
-        #
-        # if blinking_ratio > 5.7:
-        #     cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
-
-
-# Gaze detection
+        # Gaze detection
         gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
         gaze_ratio_right_eye = get_gaze_ratio([42, 43, 44, 45, 46, 47], landmarks)
         gaze_ratio = (gaze_ratio_right_eye + gaze_ratio_left_eye) / 2
 
-        if gaze_ratio <= 1:
+        if gaze_ratio <= 0.5:
             cv2.putText(frame, "RIGHT", (50, 100), font, 2, (0, 0, 255), 3)
-            new_frame[:] = (0, 0, 255)
-        elif 1 < gaze_ratio < 2:
+        elif 0.5 < gaze_ratio < 2.5:
             cv2.putText(frame, "CENTER", (50, 100), font, 2, (0, 0, 255), 3)
         else:
-            new_frame[:] = (255, 0, 0)
             cv2.putText(frame, "LEFT", (50, 100), font, 2, (0, 0, 255), 3)
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("New frame", new_frame)
 
-    key = cv2.waitKey(1)
-    if key == 27:
+    if cv2.waitKey(1) == 27:
         break
 
 cap.release()
+cap2.release()
 cv2.destroyAllWindows()
-# import cv2
-#
-# #웹캠에서 영상을 읽어온다
-# cap = cv2.VideoCapture(0)
-# cap.set(3, 640) #WIDTH
-# cap.set(4, 480) #HEIGHT
-#
-# #얼굴 인식 캐스케이드 파일 읽는다
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#
-# while(True):
-#     # frame 별로 capture 한다
-#     ret, frame = cap.read()
-#
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-#
-#     #인식된 얼굴 갯수를 출력
-#     print(len(faces))
-#
-#     # 인식된 얼굴에 사각형을 출력한다
-#     for (x,y,w,h) in faces:
-#          cv2.rectangle(frame,(x,y),(x+w,y+h),(255, 0, 0), 2)
-#
-#     #화면에 출력한다
-#     cv2.imshow('frame',frame)
-#     if cv2.waitKey(1) == 27:
-#         break
-#
-# cap.release()
-# cv2.destroyAllWindows()
-
-# USAGE
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
-
-# import the necessary packages
+print(face_frequency)
